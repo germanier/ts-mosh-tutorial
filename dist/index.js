eff56a70f599e0e6de1df1154eb454e3fdf02905
Marco Germanier <marco@mrgt.me>
2022-07-14 23:42:07 +0100

    went through all videos from 1.1 through 4.8 incl.

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var _a;
+let age = 20;
+if (age < 50)
+    age += 10;
+let sales = 123456789;
+let course = 'ts';
+let is_free = true;
+let level;
+function render(document) {
+    console.log(document);
+}
 let numbers = [1, 2, 3];
 function calculateTax(income, taxYear = 2022) {
     let taxRate = 0.0, tax = 0;
@@ -18,5 +29,38 @@ let employee = {
     retire: (date) => {
         console.log(date);
     }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+if (customer !== null && customer !== undefined)
+    console.log(customer.birthday);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let logo = null;
+logo === null || logo === void 0 ? void 0 : logo('a');
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+// initial types
+let age: number = 20;
+if (age < 50)
+    age +=10;
+
+let sales = 123456789;
+let course = 'ts';
+let is_free = true; 
+let level;
+
+function render(document: any) {
+    console.log(document);
+}
+
+//list of items
 let numbers:number[] = [1, 2, 3]
-
+// enums - k/v
 const enum Size {Small = 1, Medium, Large}
-
+// function
 function calculateTax(income: number, taxYear: number = 2022): number {
     let taxRate = 0.0, tax = 0
     while (income > 0) {
@@ -15,6 +30,7 @@ function calculateTax(income: number, taxYear: number = 2022): number {
 }
 calculateTax(20000)
 
+// objects v1
 let employee: {
     readonly id: number,
     name: string
@@ -25,6 +41,80 @@ let employee: {
     retire: (date:Date) => {
         console.log(date);
     }
-
 };
 
+// objects v2 . DRY // create custom type
+type Employee = {
+    readonly id: number,
+    name: string
+    retire: (date: Date) => void    
+}
+// union type
+function kgToLbs(weight:number | string): number {
+    if (typeof weight === 'number') {
+        return weight * 2.2
+    }
+    else {
+        return parseInt(weight) * 2.2
+    }
+}
+
+// intersection
+type Draggable = {
+    drag: () => void
+};
+type Resizable = {
+    resize: () => void
+};
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+// literal types:  can only be discrete, one of two values, say.
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
+// nullable  type
+function greet(name: string | null | undefined) {  
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
+greet(undefined)
+
+// optional chaining
+
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date()};
+}
+// bad implementation
+let customer = getCustomer(0);
+if (customer !== null && customer !==undefined)
+console.log(customer.birthday);
+
+// better implementation : optional property access
+console.log(customer?.birthday?.getFullYear())
+
+// optional element access operator
+//customers?.[0]
+
+//optional call
+let logo: any = null
+logo?.('a');
+
+let speed:  number | null = null;
+let ride = {
+    // Falsy values in js: (undefined, null, '', false, 0)
+//    speed: speed || 30 // problem is 0 is cast as 30 in this way!
+    speed: speed ?? 30 // this is better because doesnt have the above error
+}
